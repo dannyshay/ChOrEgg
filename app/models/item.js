db4405320a6cee37da0629d2b2b1827140609fbd
Danny Shay <danny624@gmail.com>
2016-03-19 13:07:14 -0400

    * Got front-end angular code working as well - needs mongo back end

--- app/models/item.js
+++ app/models/item.js
@@ -1,9 +1,15 @@
 var mongoose = require('mongoose');
 
+var Schema = mongoose.Schema;
+
+var itemSchema = new Schema({
+   name: String,
+   date: Date,
+   image: String
+});
+
+var Item = mongoose.model('item',itemSchema);
+
 // define our item model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('item', {
-   name: {type: String, default: ''},
-   date: {type: Date, default: '1/1/0001'},
-   image: {type: String, default: ''}
-});
+module.exports = Item;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express         = require('express');
 var app             = express();
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
+var mongoose        = require('mongoose');
 
 // configuration =================================================
 
@@ -16,7 +17,7 @@ var port = process.env.PORT || 8080;
 
 // connect to our mongoDB database
 // (uncomment after we setup mongoDB)
-//mongoose.connect(db.url);
+mongoose.connect(db.url);
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json
