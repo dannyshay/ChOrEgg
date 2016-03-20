cf70a7054245def36f7e9b9df1d3218d6be72a6c
Danny Shay <danny624@gmail.com>
2016-03-20 10:57:10 -0400

    * Use live MongoDB site

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // config/db.js
 module.exports = {
-    url : 'mongodb://localhost/ChOrEgg'
-    //url : 'mongodb://app_ChOrEggWeb:!lzUTu%D9NpN@ds015849.mlab.com:15849/choregg'
+    //url : 'mongodb://localhost/ChOrEgg'
+    url : 'mongodb://app_ChOrEggWeb:!lzUTu%D9NpN@ds015849.mlab.com:15849/choregg'
 }
