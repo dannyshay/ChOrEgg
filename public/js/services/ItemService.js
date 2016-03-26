59bab3517452247c33438ecce96bf89f77e081e2
Danny Shay <danny624@gmail.com>
2016-03-26 13:22:42 -0400

    * Add some more unit tests for APIs

--- public/js/services/ItemService.js
+++ public/js/services/ItemService.js
@@ -4,19 +4,6 @@ angular.module('ItemService', []).factory('Item', ['$http', function($http) {
         // call to get all items
         get : function() {
             return $http.get('/api/items');
-        },
-
-            // these will work when more API routes are defined on the Node side of the
-        //call to POST and create a new item
-
-        create : function(itemData) {
-            return $http.post('/api/items', itemData);
-        },
-
-        // call to DELETE an item
-
-        delete : function(id) {
-            return $http.delete('/api/items/' + id);
         }
     }
 }]);
