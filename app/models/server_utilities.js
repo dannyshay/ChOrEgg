a1e6e5299a6011bdf4c2dbdd704fe85a5ed1feb1
Danny Shay <danny624@gmail.com>
2016-04-04 21:58:57 -0400

    * Added test for api/items/getTwoRandomItems

--- app/models/server_utilities.js
+++ app/models/server_utilities.js
@@ -1,5 +1,44 @@
 module.exports = {
-    getRandomInt : function(min, max) {
+    getRandomInt: function (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+
+    download: function (uri, filename, callback) {
+        request.head(uri, function (err, res, body) {
+            //console.log('content-type:', res.headers['content-type']);
+            //console.log('content-length:', res.headers['content-length']);
+
+            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        });
+    },
+
+    cropImageToBounds: function (myImgPath, myMinImgPath, width, height) {
+        gm(myImgPath)
+            .size(function (err, size) {
+                var posX = 0;
+                var posY = 0;
+                var cropwidth = width;
+                var cropheight = height;
+
+                //Landscape image
+                if (size.width > size.height) {
+                    posX = (size.width - size.height) / 2;
+                    posY = 0;
+                    cropwidth = size.height;
+                    cropheight = size.height;
+                } else { // Portrait Image
+                    posX = 0;
+                    posY = (size.height - size.width) / 2;
+                    cropwidth = size.width;
+                    cropheight = size.width;
+                }
+
+                gm(myImgPath)
+                    .crop(posX, posY, cropwidth, cropheight)
+                    .write(myMinImgPath, function (err) {
+                        if (!err) console.log('done');
+                        if (err) console.log(err);
+                    });
+            });
     }
-}
+};
