697c65a7b3f52aa51c9b84eedbc9ac77d5e70e68
Danny Shay <danny624@gmail.com>
2016-09-18 11:21:43 -0400

    Try to add loading flag to the Splash controller itself

--- public/app/controllers/SplashController.js
+++ public/app/controllers/SplashController.js
@@ -1,7 +1,15 @@
 angular
     .module('choregg')
-    .controller('SplashController', ["$scope", 'StateService', function($scope, StateService) {
+    .controller('SplashController', ["$scope", 'StateService', 'LoadingService', function($scope, StateService, LoadingService) {
         StateService.setCurrentState('splash');
+
+        $scope.$watch(function() {return LoadingService.getLoading();},
+            function(aLoading) {
+                if(aLoading != null && aLoading != $scope.loading) {
+                    $scope.loading = aLoading;
+                }
+            }
+        );
 
         $scope.$watch(function() {return StateService.getCurrentState();},
             function(aState) {
