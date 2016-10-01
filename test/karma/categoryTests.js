245aeb4c097d2c4fe02002cf212f2945ac62e85c
Danny Shay <danny624@gmail.com>
2016-10-01 12:00:05 -0400

    Adding back Karma

--- test/karma/categoryTests.js
+++ test/karma/categoryTests.js
@@ -4,21 +4,9 @@ describe('Category Service', function(){
 
     beforeEach(angular.mock.module('choregg'));
 
-    beforeEach(function(done) {
-        inject(function($httpBackend, $rootScope){
-            inject(function(_CategoryService_) {
-                CategoryService = _CategoryService_;
-                CategoryService.loadCategories().then(function() {
-                    done();
-                });
-
-                $httpBackend.when('GET','/api/categories').respond([{"categoryName":"People"},{"categoryName":"Inventions"},{"categoryName":"Monuments"}])
-                $httpBackend.when('GET','views/main.html').respond(200);
-
-                $rootScope.$apply();
-            });
-        });
-    });
+    beforeEach(inject(function(_CategoryService_) {
+        CategoryService = _CategoryService_;
+    }));
 
     it('exists', function() {
        expect(CategoryService).toBeDefined();
