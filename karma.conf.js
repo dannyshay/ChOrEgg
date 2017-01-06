3c477241481e25a09d99c2eb6e7df70d1617d0d6
Danny Shay <dannyshay@Dannys-MBP.hsd1.ga.comcast.net>
2017-01-05 20:56:25 -0500

    More clover updates for karma

--- karma.conf.js
+++ karma.conf.js
@@ -58,14 +58,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'public/app/**/*.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress','coverage'],
 
+    coverageReporter: {
+        reporters: [
+            {type: 'text-summary'},
+            {type: 'clover', dir: 'test-reports', subdir: '.', file: 'clover.xml'}
+        ]
+    },
 
     // web server port
     port: 9876,
