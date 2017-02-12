51e1c7929056c04a628699a65670d9b3dfd741f3
Danny Shay <danny624@gmail.com>
2017-02-12 15:53:34 -0500

    CHOREGG-25 * Convert categories to dropdown

--- public/app/services/ItemService.js
+++ public/app/services/ItemService.js
@@ -45,6 +45,8 @@ angular
                             itemNames = items[0].itemSet.map(function(i) { return i.name});
                         }
 
+                        console.log("aCategory = " + aCategory);
+
                         // Call the API to get some more items as specified by the input params
                         choreggAPI.GetItemsInTimespan.query({ category: aCategory, timeSpan: aTimespan, numPairs: aNumPairs, anOldItemSet: JSON.stringify(itemNames)},
                             function (data) {
